b4f772bee55d2420bb42eb019f70e111f5aad7a5
JinHyoukYork <jdd04026@gmail.com>
2016-07-25 19:42:55 +0900

    add checkEmail in login.js

--- Login.js
+++ Login.js
@@ -13,24 +13,13 @@ var userListPool = mysql.createPool({
 	password: 'Despair$667',
 	database: 'UserList'
 });
-
 Loginserver.listen(port);
-
-/*connection.connect(function(err) {
-    if (err) {
-        console.error('mysql connection error');
-        console.error(err);
-        throw err;
-    }
-});*/
-
 // Routing
 app.use(express.static(path.join(__dirname,'public')));
-
 // Login code
 io.sockets.on('connection', function (socket) {
 	socket.on('sign_in',function(userdata_from){
-		function findEmail(data, callback){
+		function signin(data, callback){
 			userListPool.query('SELECT Email FROM UserInfo WHERE Email = ?',userdata_from,function(err,rows) {
 				if (err) {
 					callback(err,null);
@@ -41,35 +30,32 @@ io.sockets.on('connection', function (socket) {
 			});
 		}
 		var connect_status = 0;
-		findEmail(data, function(err, useremail){
-			if (err) {
+		signin(data, function(err, useremail){
+			if(err){
 				socket.emit('login',{
 					connect_status: 0
 				});
-			} else {
-				if(contents.length>0){
+			}else{
+				if(useremail.length>0){
 					function socketinput(callback){//소켓 설정
-							socket.useremail=useremail;
-								socket.key=random();
-								callback(socket.key);
+						socket.useremail=useremail;
+							socket.key=random();
+							callback(socket.key);
 					}
-					socketinput(function(key){
-							userListPool.query('UPDATE isConnect FROM UserInfo SET isConnect =? WHERE Email =?',[key,useremail],function{
-									socket.emit('login',{
-										connect_status: 1,
-										pushemail: useremail
-										yourkey: key
-								});
-								userListPool.release();
+					socketinput(function(key) {
+						userListPool.query('UPDATE isConnect FROM UserInfo SET isConnect =? WHERE Email =?',[key,useremail],function{
+								socket.emit('login',{
+									connect_status: 1,
+									pushemail: useremail,
+									yourkey: key
 							});
+							userListPool.release();
+						});
 					});
-
-
-
-				} else if(contents.length == 0) {
+				}else if(!useremail.length){
 					socket.emit('login',{
 						connect_status: 0,
-						pushemail: contents
+						pushemail: useremail
 					});
 				}
 			}
@@ -77,19 +63,46 @@ io.sockets.on('connection', function (socket) {
 	});
 	socket.on('disconnect',function(){
 			if(!(!socket.username)){
-			}
-			else{
+			}else{
 			userListPool.query('UPDATE isConnect FROM UserInfo SET isConnect =? WHERE Email =?',[0,socket.useremail]);
 			}
 	});
-	socket.on('sign_up',function(userdata_from)){
-		function
-	}
+	socket.on('sign_up',function(userdata_from){
+	});
+	socket.on('check_email',function(useremail_from){
+		function check_email(data, callback){
+			userListPool.query('SELECT Email FROM UserInfo WHERE Email = ?',useremail_from,function(err,rows){
+				if(err){
+					callback(err,null);
+				}else{
+					callback(null,rows[0].Email);
+				}
+				userListPool.release();
+			});
+		}
+		check_email(data, function(err, useremail){
+			if(err){
+				socket.emit('email_check',{
+					connect_status: 0
+				});
+			}else{
+				if(useremail.length>0){
+					socket.emit('email_check',{
+						connect_status: 0
+					});
+				}else if(!useremail.length){
+					socket.emit('email_check',{
+						connect_status: 1,
+						pushemail: useremail
+					});
+				}
+			}
+		});
+	});
 });
 function random () {
 	var RandomNumber=0;
 	while(RandomNumber==0)
-	 RandomNumber=Math.random() * (127+128) -128;
-  return RandomNumber;
-
+	RandomNumber=Math.random() * (127+128) -128;
+  	return RandomNumber;
 }
