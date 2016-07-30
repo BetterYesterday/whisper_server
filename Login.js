ee612291d86d275d8364e85407104f00369a8fd2
JinHyoukYork <jdd04026@gmail.com>
2016-07-31 08:11:16 +0900

    login mod13

--- Login.js
+++ Login.js
@@ -18,17 +18,9 @@ Loginserver.listen(port);
 app.use(express.static(path.join(__dirname,'public')));
 // Login code
 io.sockets.on('connection', function (socket) {
-	socket.emit('login',{
-			connect_status: 1,
-			pushemail: 'wrong'
-	});
 	socket.on('sign_in',function(userdata_from){
 		Email = userdata_from[0];
 		Password = userdata_from[1];
-		socket.emit('login',{
-			connect_status: 1,
-			pushemail: 'wrong'
-		});
 		function signin(data, callback){
 			userListPool.query('SELECT * FROM UserInfo WHERE Email = ?',
 				Email,function(err,rows) {
