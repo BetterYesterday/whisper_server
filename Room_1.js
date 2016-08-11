ca5e2330593abb5ddf0a34eb6d4b58c04adbf770
kdve <clplanet26@gmail.com>
2016-08-11 13:50:20 +0900

    Room_Completed?

--- Room_1.js
+++ Room_1.js
@@ -40,6 +40,9 @@ var circular=0;
 var priority_arr=new Array();
 setInterval(function(){
 	userListPool.query('SELECT * FROM RoomCount ORDER BY Point ASC;'function(err,rows){//우선순위 받아옴
+		for(circular=0;rows[circular].Point<0;circular++){
+
+		}
 		priority_arr=rows;
 	});
 }, 60000);
@@ -55,47 +58,73 @@ io.sockets.on('connection', function (socket) {//소켓 연결
 	socket.emit('chat_message',{message:'Room_Connected'});
 
 	socket.on('sign_in',function(userdata_from){//Email,key
-  userListPool.query('SELECT * FROM UserInfo WHERE Email = ?',data.useremail,function(err,rows) {
+  userListPool.query('SELECT * FROM UserInfo WHERE Email = ?',userdata_from.Email,function(err,rows) {
     if (err||!rows[0].isConnect) {
 			socket.emit('chat_message',{roomstatus:"ERR"});
 			//Logger써서 에러출력
-		} else if(data.userkey != rows[0].isConnect){
+		} else if(userdata_from.key != rows[0].isConnect){
 			socket.emit('chat_message',{roomstatus:"Canceled"});
 			//로그인 실패(공격시도) -> 해당 ip 차단(일시적)
 		} else{
 			socket.id=userdata_from.Email;
 			socket.emit('chat_message',{roomstatus:"connected"});
+			var timerid;
+			var minutesave;
+			socket.on('room_push',function(dmdkdkdk){//새로운 방 받지 않음 기능
+																								//yn:yes(1),no(0) minute:몇분?
+				if(dmdkdkdk.yn){
+					userListPool.query('SELECT * FROM RoomCount WHERE Email = ?',[socket.id],function(err,rrows){
+							userListPool.query('UPDATE RoomCount SET Point=? WHERE Email = ?',[-dmdkdkdk.minute,socket.id],function(){
+								priority_arr[priority_arr.indexOf(rrows[0])]=undefined;
+								minutesave = rrows[0].Point;
+								timerid = setTimeout(function(){
+											userListPool.query('UPDATE RoomCount SET Point=? WHERE Email = ?',[rrows[0].Point,socket.id],function(){
+
+											});
+								},dmdkdkdk.minute);
+							});
+					});
+				}else{
+					clearTimeout(timerid);
+					userListPool.query('UPDATE RoomCount SET Point=? WHERE Email = ?',[minutesave,socket.id],function(){
+
+					});
+				}
+			});
 			socket.on('room_change',function(dmdkdk){//now:현재 방 리스트 전부(,으로 구분) status:추가?제거? room:방이름(스트링,삭제 때만 사용)
 																							 //message:메시지(스트링, 개설 때만 사용)
 				if(dmdkdk.status){//랜덤 방 매칭
 				roomname++;
+				for(;priority_arr[circular]==undefined;circular++){}
  					ssocket.emit(priority_arr[circular].Email,{roomname:roomname,message:dmdkdk.message});//푸쉬에서 룸네임을 전송 명령
 	 					ssocket.emit(socket.id,{roomname:roomname});
 
 						RoomConn.write("./Rooms/"+socket.id,now.split().push(roomname).shifter().join(),function(err){
-							logger.error("write ERROR!!! "+socket.id);
-							userListPool.query('UPDATE RoomCount SET LastPushedBy=now() WHERE Email = ?',[socket.id],function(){
+							if(err){logger.error("write ERROR!!! "+socket.id);}
 
-							  userListPool.query('SELECT * FROM UserInfo WHERE Email = ?',socket.id,function(err,rows_1) {
-									rows_l.LastPushedBy
-								});
+							userListPool.query('UPDATE RoomCount SET Point=date_format(now(),"%Y%m%d%H%i%s" WHERE Email = ?',[socket.id],function(){
+
 							});
 						});
 						RoomConn.write("./Rooms/"+priority_arr[circular].Email,now.split().push(roomname).shifter().join(),function(err){
-							logger.error("write ERROR!!! "+priority_arr[circular].Email);
+							if(err){logger.error("write ERROR!!! "+priority_arr[circular].Email);}
+							userListPool.query('UPDATE RoomCount SET Point=date_format(now(),"%Y%m%d%H%i%s" WHERE Email = ?',[priority_arr[circular].Email],function(){
+
+							});
 						});
 						circular++;
 				}else{//방 삭제
-					var temparr = dmdkdk.now.split()
+					var temparr = dmdkdk.now.split();//현재 있는 채팅방에서 삭제
 					temparr[temparr.indexOf(dmdkdk.room)]=undefined;
-					userListPool.query('SELECT * FROM ')
-				}
+					RoomConn.write("./Rooms/"+priority_arr[circular].Email,temparr.shifter().join(),function(err){
+						if(err){logger.error("write ERROR!!! "+priority_arr[circular].Email);}
+					});
+				}//방 복원기능 추가 필요
 
 			});
     }
 
   });
-});
 });
 });
 });
